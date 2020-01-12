dfebc80e44e40dd75b548448dc306dda6408389f
Ansu <ansuaggarwal@yahoo.com>
2020-01-12 06:14:56 +0530

    First Commit

---
+++ run.bat
@@ -0,0 +1,17 @@
+set TRAINING_DATA=input/train_folds.csv
+set TEST_DATA=input/test.csv
+
+set MODEL=randomforest
+
+::set FOLD=0
+::python -m src.train
+::set FOLD=1
+::python -m src.train
+::set FOLD=2
+::python -m src.train
+::set FOLD=3
+::python -m src.train
+::set FOLD=4
+::python -m src.train
+
+python -m src.predict
